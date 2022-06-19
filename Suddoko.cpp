82cb3444c808e166b6fa110739aa3c5ec353806c
vaibhavjan <84914802+vaibhavjan@users.noreply.github.com>
2022-06-19 17:36:51 +0530

    Update Suddoko.cpp

---
+++ Suddoko.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool ispos(int row,int col,vector<vector<char>> & board,char c){
+        for(int i=0;i<9;i++){
+            if(board[i][col]==c)
+                return false;
+            if(board[row][i]==c)
+                return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c)
+                return false;
+        }
+        return true;
+    }
+    bool solve(vector<vector<char>> & board){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    for(char c='1';c<='9';++c){
+                        if(ispos(i,j,board,c)){
+                            board[i][j]=c;
+                            if(solve(board)==true)
+                                return true;
+                            else
+                                board[i][j]='.';
+                        }
+                    }
+                    return false;
+                }
+        }
+        }
+        return true;
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};
